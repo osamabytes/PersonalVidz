9849154473f4986cad5fb5a4f455e8f195e4cd95
OsamaBytes <osama.ahmed@46bytes.com>
2022-09-26 06:48:33 +0500

    Created Models and Navigation Models in the Model Services and Created DbContext with TUser, TRole, and Guid as its key.

---
+++ PersonalVidz/Data/PersonalVidzDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Model.Identity;
+using Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class PersonalVidzDbContext: IdentityDbContext<User, ApplicationRole, Guid>
+    {
+        public PersonalVidzDbContext(DbContextOptions<PersonalVidzDbContext> options): base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+        }
+    }
+}
